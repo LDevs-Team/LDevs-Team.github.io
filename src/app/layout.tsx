080a73f1128c2c7aa601abb2744b3a3f48b5c8ee
StarNumber <64470722+StarNumber12046@users.noreply.github.com>
2023-09-09 10:23:35 +0200

    fix: fixed relative font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,10 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'LDevs',
@@ -20,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
